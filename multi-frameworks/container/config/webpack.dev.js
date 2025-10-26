93eac7859719f70fd35e037451236c648ddb5e82
krzvcho <krzysztof.piszczek@gmail.com>
2025-10-26 18:23:42 +0100

    container deployment actions

--- multi-frameworks/container/config/webpack.dev.js
+++ multi-frameworks/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common.js');
@@ -20,10 +20,7 @@ const devConfig = {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
       },
       shared: ['react', 'react-dom']
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
+    })
   ],
 };
 
