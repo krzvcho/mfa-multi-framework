a74e8425411cb844a04ae2c054f4c23b2ea99b36
krzvcho <krzysztof.piszczek@gmail.com>
2025-10-28 19:10:58 +0100

    marketing app test change

--- multi-frameworks/marketing/src/components/Landing.js
+++ multi-frameworks/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Home Page v3
             </Typography>
             <Typography
               variant="h5"
