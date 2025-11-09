bdbddec0f3674905bb83a98b6bca661e1b2aa79e
krzvcho <krzysztof.piszczek@gmail.com>
2025-11-09 11:59:41 +0100

    dashboard app source

--- multi-frameworks/container/src/App.js
+++ multi-frameworks/container/src/App.js
@@ -1,13 +1,16 @@
 import React, { lazy, Suspense } from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router, Redirect } from 'react-router-dom';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'; // MUI v7 imports
+import { createBrowserHistory } from 'history';
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const MarketingAppLazy = lazy(() => import('./components/MarketingApp'));
 const AuthAppLazy = lazy(() => import('./components/AuthApp'));
+const DashboardAppLazy = lazy(() => import('./components/DashboardApp'));
 
 const theme = createTheme(); // Customize if needed
+const history = createBrowserHistory();
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = React.useState(() => {
@@ -17,22 +20,32 @@ const App = () => {
 
   React.useEffect(() => {
     sessionStorage.setItem('isSignedIn', isSignedIn);
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
   }, [isSignedIn]);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+      <Router history={history}>
+        <Header
+          isSignedIn={isSignedIn}
+          onSignOut={() => setIsSignedIn(false)}
+        />
         <Suspense fallback={<Progress />}>
           <Switch>
-            <Route path="/auth" component={AuthAppLazy} >
+            <Route path="/auth">
               <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
+            </Route>
+            <Route path="/dashboard">
+              {!isSignedIn && <Redirect to="/" />}
+              <DashboardAppLazy />
             </Route>
             <Route path="/" component={MarketingAppLazy} />
           </Switch>
         </Suspense>
-      </BrowserRouter>
+      </Router>
     </ThemeProvider>
   );
 };
---
+++ multi-frameworks/dashboard/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const webpackCommon = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const { commonConfig } = webpackCommon;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/dashboard/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
+      },
+       shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
